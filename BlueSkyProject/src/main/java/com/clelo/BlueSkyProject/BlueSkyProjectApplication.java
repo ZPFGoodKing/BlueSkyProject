c934bc573b5f7a90eb82d0ea0c0118bf2f122026
ZPFGoodKing <34934032+ZPFGoodKing@users.noreply.github.com>
2019-03-20 14:04:45 +0800

    Add files via upload

---
+++ BlueSkyProject/src/main/java/com/clelo/BlueSkyProject/BlueSkyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.clelo.BlueSkyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueSkyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueSkyProjectApplication.class, args);
+	}
+
+}
